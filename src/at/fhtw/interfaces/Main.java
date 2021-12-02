1899b7699b7c18ceb62c983b5c3387981b524ab7
Markus Holzer <markus.holzer@hytech.at>
2021-12-02 12:02:52 +0100

    extend with collection and streams

--- src/at/fhtw/interfaces/Main.java
+++ src/at/fhtw/interfaces/Main.java
@@ -1,38 +1,76 @@
 package at.fhtw.interfaces;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
-        Shape s1 = new Square(new Position(), 5);
-        Shape s2 = new Square(new Position(), 10);
-        Shape s3 = new Rectangle(new Position(), 2, 6);
+        Collection<Shape> shapes = new ArrayList<>();
+        shapes.add(createSquare(5));
+        shapes.add(createSquare(10));
+        shapes.add(createRectangle(2, 6));
 
-        s1.display();
-        s2.display();
-        s3.display();
+        // display
+        shapes.stream().forEach(Shape::display);
+//        for (Shape s : shapes) {
+//            s.display();
+//        }
 
-        if (s1 instanceof  Scalable) {
-            ((Scalable) s1).scale(2);
-        }
-        if (s1 instanceof Moveable) {
-            System.out.println("s1 is moveable");
-        }
-        if (s1 instanceof Transformable) {
-            System.out.println("s1 is transformable");
-        }
-        if (s2 instanceof  Scalable) {
-            ((Scalable) s2).scale(2);
-        }
-        if (s3 instanceof  Scalable) {
-            ((Scalable) s3).scale(2);
-        }
+        // scale
+        scale(shapes, 2);
 
-        System.out.println("*************************");
+        System.out.println("**************************");
 
-        s1.display();
-        s2.display();
-        s3.display();
+        // display
+        shapes.stream().forEach(Shape::display);
+//        for (Shape s : shapes) {
+//            s.display();
+//        }
+
+        System.out.println("**************************");
+        double minArea = 30;
+        System.out.printf("shapes with area more then %f:\n", minArea);
+        getShapesGreaterThenAreaOf(shapes, minArea).forEach(Shape::display);
 
 
     }
+
+    private static void scale(Collection<Shape> shapes, double factor) {
+        shapes.stream().filter(s -> s instanceof Scalable)
+                .forEach(s -> ((Scalable) s).scale(factor));
+//        for (Shape s : shapes) {
+//            if (s instanceof Scalable) {
+//                ((Scalable) s).scale(factor);
+//            }
+//        }
+    }
+
+    private static Collection<Shape> getShapesGreaterThenAreaOf(Collection<Shape> shapes, double minArea) {
+        return shapes.stream()
+                .filter(s -> s.getArea() > minArea)
+                .sorted(Comparator.comparing(Shape::getArea).reversed())
+                .collect(Collectors.toList());
+    }
+
+    private static Collection<Shape> getShapesGreaterThenAreaOf_OldStyle(Collection<Shape> shapes, double minArea) {
+        Collection<Shape> selectedShapes = new ArrayList<>();
+        for (Shape s : shapes) {
+            if (s.getArea() > minArea) {
+                selectedShapes.add(s);
+            }
+        }
+        return selectedShapes;
+    }
+
+    private static Shape createSquare(double width) {
+        return new Square(new Position(), width);
+    }
+
+    private static Shape createRectangle(double width, double length) {
+        return new Rectangle(new Position(), width, length);
+    }
+
 }
